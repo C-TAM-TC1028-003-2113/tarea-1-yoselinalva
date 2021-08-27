1e0dd7e57691854e2e49eb92f111f062c3fde66b
yoseealva <A01412634@itesm.mx>
2021-08-27 12:01:43 -0500

    Terminé el trabajo de PendienteLinea a las 12:01 pm

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -1,6 +1,13 @@
 def main():
-    # escribe tu código abajo de esta línea
-    pass
+    # Datos iniciales
+    x1 = float(input("Dame x1: "))
+    y1 = float(input("Dame y1: "))
+    x2 = float(input("Dame x2: "))
+    y2 = float(input("Dame y2: "))
+    # Calculo de la pendiente
+    m = (y2 - y1) / (x2 - x1)
+    # Muestro del resultado final al usuario
+    print("Pendiente:", m)
 
 
 if __name__ == '__main__':
